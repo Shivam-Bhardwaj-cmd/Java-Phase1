760791a8320ec04f31abcf53396b5ecba2c0ffd9
Shivam Bhardwaj <your_email@example.com>
2025-09-08 06:34:37 +0000

    Added Day 1 Java programs: HelloWorld, VariablesExample, InputExample, IfElseExample

---
+++ VariablesExample.java
@@ -0,0 +1,13 @@
+public class VariablesExample {
+    public static void main(String[] args) {
+        int age = 25;
+        double salary = 55000.50;
+        String name = "Shivam";
+        boolean isLearning = true;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Learning: " + isLearning);
+    }
+}
