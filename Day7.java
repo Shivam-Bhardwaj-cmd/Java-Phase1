23c946ba4344abaf59a3d5603477bb4e1c85c73e
Shivam Bhardwaj <your_email@example.com>
2025-09-10 05:48:12 +0000

    Day 7 Problem 3: Check symmetric matrix

--- Day7.java
+++ Day7.java
@@ -5,59 +5,37 @@ public class Day7 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Input the size of the matrix
+        // Input size of the matrix
         int n = sc.nextInt();
-        int m = sc.nextInt();
-        int[][] matrix = new int[n][m];
+        int[][] matrix = new int[n][n];
 
         // Input the matrix elements
         for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
+            for(int j = 0; j < n; j++) {
                 matrix[i][j] = sc.nextInt();
             }
         }
 
-        // Call the spiral traversal method
-        spiralTraversal(matrix, n, m);
+        // Check if symmetric and print the result
+        if(isSymmetric(matrix, n)) {
+            System.out.println("Symmetric");
+        } else {
+            System.out.println("Not Symmetric");
+        }
 
         sc.close();
     }
 
-    // Method to print the spiral order of the matrix
-    public static void spiralTraversal(int[][] matrix, int n, int m) {
-        int top = 0, bottom = n - 1;
-        int left = 0, right = m - 1;
-
-        while(top <= bottom && left <= right) {
-
-            // Traverse from left to right
-            for(int j = left; j <= right; j++) {
-                System.out.print(matrix[top][j] + " ");
-            }
-            top++;
-
-            // Traverse from top to bottom
-            for(int i = top; i <= bottom; i++) {
-                System.out.print(matrix[i][right] + " ");
-            }
-            right--;
-
-            // Traverse from right to left if still rows remain
-            if(top <= bottom) {
-                for(int j = right; j >= left; j--) {
-                    System.out.print(matrix[bottom][j] + " ");
+    // Method to check if the matrix is symmetric
+    public static boolean isSymmetric(int[][] matrix, int n) {
+        for(int i = 0; i < n; i++) {
+            for(int j = i + 1; j < n; j++) {
+                if(matrix[i][j] != matrix[j][i]) {
+                    return false;
                 }
-                bottom--;
-            }
-
-            // Traverse from bottom to top if still columns remain
-            if(left <= right) {
-                for(int i = bottom; i >= top; i--) {
-                    System.out.print(matrix[i][left] + " ");
-                }
-                left++;
             }
         }
+        return true;
     }
 }
 
